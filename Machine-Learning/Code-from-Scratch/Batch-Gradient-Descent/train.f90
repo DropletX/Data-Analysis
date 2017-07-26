176ea691eddd29531424edf75112674e106d9b13
DropletX <chunefung@gmail.com>
2017-07-26 15:56:18 +0800

    Update train.f90

--- Machine-Learning/Code-from-Scratch/Batch-Gradient-Descent/train.f90
+++ Machine-Learning/Code-from-Scratch/Batch-Gradient-Descent/train.f90
@@ -10,6 +10,6 @@ program main
   theta = batch_gradient_descent(g=identity,x=x,y=y,tol=0.1D0,rate=0.01D0,max_iter=10000)
   write(*,"(A,F8.3)")"Intercept:",theta(1)
   do i=2,dims(2)
-    write(*,"(A,I2,A,F5.3)")"Parameter",i,": ",theta(i)
+    write(*,"(A,I2,A,F5.3)")"Parameter",i-1,": ",theta(i)
   end do
 end program main
